06f7837a3596f62325dc440f3329e91899f6a43b
ilnur-Git <mazel08@yandex.ru>
2023-03-02 01:09:57 +0300

    Домашка 3й семинар

---
+++ task19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i ++)
+    while(number[i] == number[length - 1])
+
+{
+    return true;
+}
+return false;
+}
+
+if (IsPalindrome()) System.Console.WriteLine($"Введенное число - {number} - является палиндромом");
+else System.Console.WriteLine($"Число {number} не является палиндромом");
+System.Console.WriteLine();
+
+
+
